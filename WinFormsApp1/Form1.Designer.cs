a1504917a51ed312d07137b06a8df3d2c9dab6c9
Erbellaco419 <ruizcaleron@gmail.com>
2024-02-15 14:44:31 +0100

    aplicacion

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,44 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            label4 = new Label();
+            label5 = new Label();
+            linkLabel2 = new LinkLabel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(427, 168);
-            button1.Name = "button1";
-            button1.Size = new Size(147, 80);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(338, 269);
+            label1.Font = new Font("Century Gothic", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(294, 23);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 1;
-            label1.Text = "label1";
+            label1.Size = new Size(178, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Formulario";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(294, 75);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(156, 146);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Black;
+            panel1.Location = new Point(210, 285);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(300, 3);
+            panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Black;
+            panel2.Location = new Point(210, 376);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(300, 3);
+            panel2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Lime;
+            button1.FlatAppearance.BorderColor = Color.Lime;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Century Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(249, 423);
+            button1.Name = "button1";
+            button1.Size = new Size(223, 40);
+            button1.TabIndex = 5;
+            button1.Text = "Entrar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.Location = new Point(439, 497);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(79, 17);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Registrarme";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(202, 497);
+            label4.Name = "label4";
+            label4.Size = new Size(231, 17);
+            label4.TabIndex = 7;
+            label4.Text = "¿No tienes cuenta creada? Dale aquí";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(219, 528);
+            label5.Name = "label5";
+            label5.Size = new Size(185, 17);
+            label5.TabIndex = 8;
+            label5.Text = "¿Has olvidado la contraseña?";
+            label5.Click += label5_Click;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel2.Location = new Point(410, 528);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(96, 17);
+            linkLabel2.TabIndex = 9;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Recuerdamela";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Century Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(210, 244);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(300, 34);
+            textBox1.TabIndex = 10;
+            textBox1.Text = "Usuario";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Century Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(210, 335);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(300, 34);
+            textBox2.TabIndex = 11;
+            textBox2.Text = "Contraseña";
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(505, 345);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(37, 34);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 12;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
+            BackColor = Color.White;
+            ClientSize = new Size(742, 567);
+            Controls.Add(pictureBox2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(linkLabel2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(linkLabel1);
             Controls.Add(button1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Font = new Font("Times New Roman", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Login Form";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
         private Label label1;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+        private LinkLabel linkLabel1;
+        private Label label4;
+        private Label label5;
+        private LinkLabel linkLabel2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private PictureBox pictureBox2;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -6,5 +6,65 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = true;
+            textBox2.Visible = true;
+
+            // Enfoca el TextBox
+            textBox2.Focus();
+        }
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            // Cuando el TextBox pierde el foco, oculta el TextBox y restaura el PasswordChar
+            textBox2.Visible = false;
+            textBox2.UseSystemPasswordChar = false;
+
+            // Aquí puedes realizar alguna acción adicional, si es necesario
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = !textBox2.UseSystemPasswordChar;
+        }
     }
 }
